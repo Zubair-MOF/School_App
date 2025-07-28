31a6740a0c7105c4f1e1f7ae3ca6d2f21b94fe6a
Zubair-MOF <mof.database1@gmail.com>
2025-07-28 11:13:14 +0430

    package.json app.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,38 @@
-const {Sequelize,DataTypes} = require('sequelize')
+// const {Sequelize,DataTypes} = require('sequelize')
 
-//db connection string 
-const sequelize = new Sequelize('school', 'root', 'khairy@123', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+// //db connection string 
+// const sequelize = new Sequelize('school', 'root', 'khairy@123', {
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
 
-//db conection status 
+// //db conection status 
+// sequelize.sync()
+// .then(() => console.log('Database & tables synced'))
+// .catch(err => console.error('Error syncing database:', err));
+
+
+//module.exports = sequelize;
+
+
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false
+  }
+);
+
 sequelize.sync()
-.then(() => console.log('Database & tables synced'))
-.catch(err => console.error('Error syncing database:', err));
-
-
+  .then(() => console.log('Database & tables synced'))
+  .catch(err => console.error('Error syncing database:', err));
 
 module.exports = sequelize;
+
